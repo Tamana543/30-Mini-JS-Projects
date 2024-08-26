21f010699d9e3ed00f69e29bb6f5134e74cd8418
Tamana543 <tamanafarzami33@gmail.com>
2024-08-26 09:55:08 +0430

    Functionality added

--- Project-3/script.js
+++ Project-3/script.js
@@ -1,3 +1,10 @@
 const input = document.getElementById("text_input"),
   counter = document.getElementById("counter");
 let curText = 0;
+
+document.addEventListener("keypress", (event) => {
+  if (event.key) curText++;
+  else curText;
+  console.log(event.key);
+  counter.textContent = +curText;
+});
