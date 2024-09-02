a7c99a69d2e8f33f7556d038cd6202ba5bc321c9
Tamana543 <tamanafarzami33@gmail.com>
2024-09-03 00:44:25 +0430

    Debugging completed

--- Project-7/script.js
+++ Project-7/script.js
@@ -2,9 +2,12 @@ const inputValue = document.getElementById("date_input"),
   dayEl = document.getElementById("day"),
   hourEl = document.getElementById("hour"),
   minEl = document.getElementById("min"),
-  secEl = document.getElementById("sec");
+  secEl = document.getElementById("sec"),
+  heading = document.querySelector(".heading"),
+  body = document.querySelector("body");
 
 [dayEl, hourEl, minEl, secEl].forEach((el) => (el.textContent = " "));
+let time;
 function timerEn() {
   const givenDate = new Date(inputValue.value);
   const date = new Date();
@@ -12,7 +15,7 @@ function timerEn() {
   let hour = date.getHours();
   let min = date.getMinutes();
   let day = date.getDate();
-  setInterval(function () {
+  time = setInterval(function () {
     // debugger;
     sec--;
     if (sec < 0) {
@@ -30,7 +33,15 @@ function timerEn() {
 
     let main = Math.abs(day - givenDate.getDate());
 
-    if (main < 0) clearInterval(timerEn);
+    if (main < 0) {
+      dayEl.textContent = 0;
+      hourEl.textContent = 0;
+      minEl.textContent = 0;
+      secEl.textContent = 0;
+      clearInterval(time);
+      heading.textContent = "Time Is Up , Well Done 😀";
+      body.style.backgroundColor = "#ffff";
+    }
     dayEl.textContent = main;
     hourEl.textContent = hour;
     minEl.textContent = min;
@@ -38,6 +49,6 @@ function timerEn() {
   }, 1000);
 }
 inputValue.addEventListener("input", () => {
-  if (timerEn) clearInterval(timerEn);
+  if (time) clearInterval(time);
   timerEn();
 });
