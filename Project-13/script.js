aa538847e8254f8a9e5ff538d3475277dc58bdf0
Tamana543 <tamanafarzami33@gmail.com>
2024-09-09 08:55:32 +0430

    Main Functionality added

--- Project-13/script.js
+++ Project-13/script.js
@@ -1,13 +1,35 @@
 const passwordInput = document.querySelector(".password_label-input"),
   copyIcon = document.querySelector(".icon"),
-  upperCase = document.getElementById("uppercase"),
-  lowerCase = document.getElementById("lowercase"),
-  number = document.getElementById("number"),
-  symbol = document.getElementById("symbol");
+  upperCase = document.getElementById("uppercase").checked,
+  lowerCase = document.getElementById("lowercase").checked,
+  number = document.getElementById("number").checked,
+  symbol = document.getElementById("symbol").checked,
+  submitBtn = document.querySelector(".submit");
 function copyEn() {
   const passwordInputEl = passwordInput.textContent;
   navigator.clipboard.writeText(`${passwordInputEl}`);
   alert("Text Copied");
 }
+function generatPassword() {
+  let password = "";
+  const passwordMain = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseEng = String(passwordMain).toUpperCase();
+
+  const numberEn = "0 1 2 3 4 5 6 7 8 9";
+  let outpot = "";
+
+  if (upperCase) outpot += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowerCase) outpot += "abcdefghijklmnopqrstuvwxyz";
+  if (number) outpot += numberEn;
+  if (symbol) outpot += "~!@#$%^&*()_-+=,'/?<>:;";
+  for (let i = 0; i < 12; i++) {
+    let randomEng = outpot[Math.floor(Math.random() * outpot.length)];
+    password += randomEng;
+  }
+  passwordInput.textContent = password;
+  // passwordInput.textContent = outpot;
+  console.log(outpot);
+}
 copyIcon.addEventListener("click", copyEn);
 //
+submitBtn.addEventListener("click", generatPassword);
