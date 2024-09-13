2c83f3f92b56e6d47921a7b2e88b07e944c6d72f
Tamana543 <tamanafarzami33@gmail.com>
2024-09-13 11:08:23 +0430

    Bug Fixed

--- Project-16/script.js
+++ Project-16/script.js
@@ -1,15 +1,17 @@
-const tab = document.querySelectorAll(".tab"),
+const tabs = document.querySelectorAll(".tab"),
+  tabContainer = document.querySelector(".tabs_bar"),
   textBoxes = document.querySelectorAll(".text");
 
-function mainEn() {
-  tab.forEach((tab) => {
-    textBoxes.forEach((box) => {
-      if (tab.dataset.title === box.dataset.title) {
-        console.log(box);
-        box.classList.toggle("hide");
-        tab.classList.toggle("active");
-      }
-    });
-  });
+function mainEn(event) {
+  const click = event.target.closest(".tab");
+
+  if (!click) return;
+  tabs.forEach((tab) => tab.classList.remove("active"));
+  textBoxes.forEach((text) => text.classList.add("hide"));
+
+  click.classList.add("active");
+  document
+    .querySelector(`.text_cont--${click.dataset.title}`)
+    .classList.remove("hide");
 }
-tab.forEach((tab) => tab.addEventListener("click", mainEn));
+tabContainer.addEventListener("click", mainEn);
