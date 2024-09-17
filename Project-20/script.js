c343aeb83db3f078f55d4f12dbffa925e8a938c4
Tamana543 <tamanafarzami33@gmail.com>
2024-09-17 21:30:50 +0430

    Main functionality added

--- Project-20/script.js
+++ Project-20/script.js
@@ -1,2 +1,30 @@
-const allBtns = document.querySelectorAll(".btn");
-const boxes = document.querySelectorAll("span");
+const allBtn = document.querySelector(".btn"),
+  redBtn = document.querySelector(".red_btn"),
+  greenBtn = document.querySelector(".green_btn"),
+  blueBtn = document.querySelector(".blue_btn");
+const boxes = document.querySelectorAll(".box");
+const boxEl = ["red", "green", "blue"];
+allBtn.addEventListener("click", () => {
+  boxes.forEach((ele) => (ele.style.display = "inline"));
+});
+redBtn.addEventListener("click", () => {
+  boxes.forEach((ele) => {
+    ele.classList.contains("red_box")
+      ? (ele.style.display = "inline")
+      : (ele.style.display = "none");
+  });
+});
+greenBtn.addEventListener("click", () => {
+  boxes.forEach((ele) => {
+    ele.classList.contains("green_box")
+      ? (ele.style.display = "inline")
+      : (ele.style.display = "none");
+  });
+});
+blueBtn.addEventListener("click", () => {
+  boxes.forEach((ele) => {
+    ele.classList.contains("blue_box")
+      ? (ele.style.display = "inline")
+      : (ele.style.display = "none");
+  });
+});
