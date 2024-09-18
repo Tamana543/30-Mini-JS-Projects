9f9994c7d57c7c5565095ebabe60dbccc884e16f
Tamana543 <tamanafarzami33@gmail.com>
2024-09-18 17:44:00 +0430

    Js File added

---
+++ Project-21/script.js
@@ -0,0 +1,4 @@
+const beatuiful = document.querySelector(".beautifull"),
+  incridible = document.querySelector(".incridible"),
+  fantastic = document.querySelector(".fantastic"),
+  amazing = document.querySelector(".amazing");
