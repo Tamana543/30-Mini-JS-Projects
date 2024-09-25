0dd195c3f484d5d06f187e190eb9957f15ef5c9e
Tamana543 <tamanafarzami33@gmail.com>
2024-09-25 11:52:50 +0430

    Responsive features added

--- Project-23/script.js
+++ Project-23/script.js
@@ -17,33 +17,11 @@ function mainEn(event) {
     )} <button type="button" class="close">X</button></span
      >
     `;
-    // optionsBox.appendChild(html);
+
     optionsBox.insertAdjacentHTML("afterbegin", html);
   });
-
-  // array.unshift(text);
-  // const main = new Set(array);
-  // console.log(main);
-
-  // optionsBox.insertAdjacentHTML("afterbegin", html);
-
-  // main.forEach((ele, i) => {
-  //   const html = `
-  //         <span class="option flex"
-  //        >${ele} <button type="button" class="close">X</button></span
-  //         >
-  //       `;
-
-  //   optionsBox.insertAdjacentHTML("afterbegin", html);
-  // });
-  // main.shift();
-  // console.log(main);
 }
 
-// document.addEventListener("keypress", function (event) {
-//   const main = event.key;
-//   if (main === ".") mainEn(main);
-// });
 textBox.addEventListener("keyup", function (event) {
   mainEn(event.target.value);
 });
