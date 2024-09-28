e64b54652e06c136280ec441f2c5710d59d1746f
Tamana543 <tamanafarzami33@gmail.com>
2024-09-28 18:36:39 +0430

    First Functionality added

--- Project-24/script.js
+++ Project-24/script.js
@@ -1,3 +1,24 @@
 const openBtn = document.querySelector(".open_btn");
-const closeBtn = document.querySelector(".close_btn");
+// const closeBtn = document.querySelector(".close_btn");
 const links = document.querySelector(".links");
+
+openBtn.addEventListener("click", function () {
+  //   this.classList.add("spin");
+  //   this.classList.toggle("hide");
+
+  this.classList.toggle("spin");
+  this.textContent = "";
+  const span = document.createElement("span");
+  span.textContent = "X";
+  span.classList.add("close_btn");
+  this.appendChild(span);
+  //   this.textContent = `<span class="line">|</span>
+  //           <span class="line_2">|</span>`;
+  //   this.classList.toggle("hide");
+
+  //   this.style.animation = "spin 1s linear";
+});
+// closeBtn.addEventListener("click", function () {
+//   this.classList.toggle("hide");
+//   this.classList.toggle("spin");
+// });
