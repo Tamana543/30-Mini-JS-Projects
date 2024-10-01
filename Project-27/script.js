744d4cf5f77118c1acf74a21de277b1a428c2a2c
Tamana543 <tamanafarzami33@gmail.com>
2024-10-01 21:17:47 +0430

    Function Debugged

--- Project-27/script.js
+++ Project-27/script.js
@@ -2,27 +2,16 @@ const sub_imgs = document.querySelectorAll(".sub_img");
 const mainImgContainer = document.querySelector(".display_img");
 const mainImg = document.querySelector(".display_img img");
 sub_imgs.forEach((ele) => {
-  //   ele.addEventListener("mouseover", function () {
-  //     ele.style.transform = "scale3d(2, 2, 2)";
-  //     ele.style.zIndex = "111";
-  //   });
-});
-
-/*const images = document.querySelectorAll(".other__img img");
-const imgShowBox = document.querySelector(".show__img");
-const imgShow = document.querySelector(".show__img img");
-
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    imgShowBox.classList.add("active");
-    imgShow.src = `${image.src}`;
+  ele.addEventListener("click", function () {
+    mainImgContainer.classList.add("active");
+    mainImg.src = `${ele.src}`;
   });
 });
 
-imgShowBox.addEventListener("click", (e) => {
-  if (e.target.classList.contains("active")) {
-		console.log("contains");
-    imgShowBox.classList.remove("active");
-    imgShow.src = "";
+mainImgContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("active")) {
+    console.log(`Heekkdkkd`);
+    mainImgContainer.classList.remove("active");
+    mainImg.src = "";
   }
-});*/
+});
