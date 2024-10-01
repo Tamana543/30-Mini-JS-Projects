ad230ef239a91588d6118b63f0147964a8cb5fd2
Tamana543 <tamanafarzami33@gmail.com>
2024-10-01 21:11:15 +0430

    File Updated

--- Project-27/script.js
+++ Project-27/script.js
@@ -1,7 +1,28 @@
 const sub_imgs = document.querySelectorAll(".sub_img");
+const mainImgContainer = document.querySelector(".display_img");
+const mainImg = document.querySelector(".display_img img");
 sub_imgs.forEach((ele) => {
-  ele.addEventListener("mouseover", function () {
-    ele.style.transform = "scale3d(2, 2, 2)";
-    ele.style.zIndex = "111";
+  //   ele.addEventListener("mouseover", function () {
+  //     ele.style.transform = "scale3d(2, 2, 2)";
+  //     ele.style.zIndex = "111";
+  //   });
+});
+
+/*const images = document.querySelectorAll(".other__img img");
+const imgShowBox = document.querySelector(".show__img");
+const imgShow = document.querySelector(".show__img img");
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    imgShowBox.classList.add("active");
+    imgShow.src = `${image.src}`;
   });
 });
+
+imgShowBox.addEventListener("click", (e) => {
+  if (e.target.classList.contains("active")) {
+		console.log("contains");
+    imgShowBox.classList.remove("active");
+    imgShow.src = "";
+  }
+});*/
