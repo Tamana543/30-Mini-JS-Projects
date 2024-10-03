bb13351d49603c7f5aa4bdc12ef670e918cd84ad
Tamana543 <tamanafarzami33@gmail.com>
2024-10-03 16:14:46 +0430

    Create Cards Function Added

--- Project-29/Script.js
+++ Project-29/Script.js
@@ -3,6 +3,29 @@ const closeModalBtn = document.querySelector(".close_btn");
 const board = document.querySelector(".board");
 let moves = document.querySelector(".moves");
 let cardText = [];
+let moveCounter = 0;
+let match = 0;
+let isFirstClick = true;
+let timerID;
+let isRestart = false;
+let cards = [
+  "diamond",
+  "diamond",
+  "plane",
+  "plane",
+  "anchor",
+  "anchor",
+  "bolt",
+  "bolt",
+  "leaf",
+  "leaf",
+  "bicycle",
+  "bicycle",
+  "cube",
+  "cube",
+  "bomb",
+  "bomb",
+];
 
 closeModalBtn.addEventListener("click", () => {
   modal.classList.toggle("show_modal");
@@ -23,4 +46,24 @@ function shuffle(ele) {
     ele[randomIndex] = temporaryValue;
   }
   return ele;
+}
+let sguffleCards = shuffle(cards);
+function createCards() {
+  for (let card of sguffleCards) {
+    const li = document.createElement("li");
+    li.classList.toggle("card");
+    const i = document.createElement("i");
+    i.classList.toggle("fa");
+    if (card === "plane") {
+      i.classList.toggle("fa-paper-plane-o");
+    } else {
+      i.classList.toggle(`fa-${card}`);
+    }
+    const board = document.querySelector(".board");
+    li.appendChild(i);
+    board.appendChild(li);
+  }
+}
+function showCard(card) {
+  card.classList.add("show");
 }
