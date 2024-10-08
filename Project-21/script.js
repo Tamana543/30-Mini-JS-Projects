479cf4c7feee262ee0034ba0551a190dd00a3cb2
Tamana543 <tamanafarzami33@gmail.com>
2024-10-08 19:26:09 +0430

    File updated

--- Project-21/script.js
+++ Project-21/script.js
@@ -1,16 +1,20 @@
-const beatuiful = document.querySelector(".beautifull");
-const textMovedAll = document.querySelectorAll(".text_moved");
+const textMovedAll = document.getElementsByClassName("text_moved");
 
 let text = [];
 let currentLetter = 0;
 let index = 0;
 
+textMovedAll[currentLetter].style.opacity = 1;
+for (let i = 0; i < textMovedAll.length; i++) {
+  change(textMovedAll[i]);
+}
+
 function change(word) {
   let wordCont = word.innerHTML;
-  wordCont = "";
+  word.innerHTML = "";
   let letters = [];
   for (let i = 0; i < wordCont.length; i++) {
-    let letter = document.createElement("letter");
+    let letter = document.createElement("span");
     letter.className = "letter";
     letter.innerHTML = wordCont.charAt(i);
     word.appendChild(letter);
@@ -20,13 +24,6 @@ function change(word) {
 
   text.push(letters);
 }
-textMovedAll[currentLetter].style.opacity = 1;
-
-for (let i = 0; i < textMovedAll.length; i++) {
-  change(textMovedAll[i]);
-}
-let test = change(currentLetter);
-setInterval(test, 1000);
 
 //Writing Letters
 
@@ -34,14 +31,14 @@ function write() {
   let word = text[currentLetter];
 
   let newWorld =
-    currentLetter === textMovedAll.length - 1
+    currentLetter == textMovedAll.length - 1
       ? text[0]
       : text[currentLetter + 1];
 
-  for (let i = 0; i < newWorld.length; i++) {
+  for (let i = 0; i < word.length; i++) {
     animateOut(word, i);
   }
-  for (let i = 0; i < word.length; i++) {
+  for (let i = 0; i < newWorld.length; i++) {
     newWorld[i].className = "letter behind";
     newWorld[0].parentElement.style.opacity = 1;
     animateIn(newWorld, i);
@@ -59,4 +56,4 @@ function animateIn(newWorld, i) {
   }, 340 + i * 80);
 }
 write();
-// setInterval(write, 150);
+setInterval(write, 2000);
