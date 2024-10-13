177ab2f107f6482dd2ac35d509d3f40149e2be5c
Tamana543 <tamanafarzami33@gmail.com>
2024-10-13 19:40:09 +0430

    Fetching API Functionality Added

--- Project-30/Script.js
+++ Project-30/Script.js
@@ -6,3 +6,22 @@ const body = document.querySelector("body"),
   nameEl = document.querySelector(".name"),
   descriptionEl = document.querySelector(".description"),
   dateEl = document.querySelector(".date");
+
+async function UrlGenerator(MainUrl) {
+  try {
+    const url = await fetch(MainUrl);
+    const data = url.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const APIKey = `58662704857e9a3d73c2c2c365191b0d`;
+  const keyword = inputEl.value;
+  const data = UrlGenerator(
+    `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&language=en-US&page=1&include_adult=false&query=${keyword}`
+  );
+  console.log(data);
+});
